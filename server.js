b366f5fa141f2c4012d3156afe4f4a328572b87e
Paola Caceres <paolacaceresoma@gmail.com>
2023-06-08 01:59:14 -0400

    Update server.js

--- server.js
+++ server.js
@@ -16,3 +16,43 @@ const db = mysql2.createConnection (
   console.log(`You are now connected to the employee_manager_db database. \n\n 
   Welcome to the Employee Tracker. \n`)
 );
+
+function homeScreen() {
+  inquirer.prompt([
+    {
+        type: 'list',
+        name: 'dbTables',
+        choices: ['View all departments', 
+                  'View all roles', 
+                  'View all employees', 
+                  'Add a department', 
+                  'Add a role', 
+                  'Add an employee', 
+                  'Update an employee role'],
+    },
+  ]).then(({ dbTables }) => {
+    switch (dbTables) {
+      case 'View all departments':
+        viewDepts();
+        break;
+      case 'View all roles':
+        viewRoles();
+        break;
+      case 'View all employees':
+        viewEmployees();
+        break;
+      case 'Add a department':
+        addDept();
+        break;
+      case 'Add a role':
+        addRole();
+        break;
+      case 'Add an employee':
+        addEmployee();
+        break;
+      case 'Update an employee role':
+        updateEmployeeRole();
+        return;
+    }
+  });
+};
