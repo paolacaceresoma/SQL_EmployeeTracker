c69eb5e0675f996e4121f3a3f6db797b8bde6af6
Paola Caceres <paolacaceresoma@gmail.com>
2023-06-08 01:56:30 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,3 +3,16 @@ const mysql2 = require('mysql2');
 const inquirer = require('inquirer');
 const app = express();
 require('dotenv').config();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+const db = mysql2.createConnection (
+  {
+    host: 'localhost',
+    user: 'root',
+    password: process.env.PASSWORD,
+    database: 'employee_manager_db'
+  },
+  console.log(`You are now connected to the employee_manager_db database. \n\n 
+  Welcome to the Employee Tracker. \n`)
+);
